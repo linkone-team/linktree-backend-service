2fb342c9edd2f9d2857ce94d3553b9a5866a58b4
Pascal Bürklin <pascal@buerkl.in>
2023-10-12 12:45:54 +0200

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,25 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	// Spring Boot
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.security:spring-security-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Mail
+	implementation("com.postmarkapp:postmark:1.10.21")
+
+	// Database Driver
+	runtimeOnly("org.postgresql:postgresql")
+
+	// Tools
+	implementation("com.google.code.gson:gson:2.10.1")
+
+	// Code Generation
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
