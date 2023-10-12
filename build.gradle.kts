7e6fa7ec0937bf6302a1e088d49b1638e484bf26
Pascal Bürklin <pascal@buerkl.in>
2023-10-12 12:35:31 +0200

    Initialize Spring Project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,33 @@
 plugins {
-    id("java")
+	java
+	id("org.springframework.boot") version "3.1.4"
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "de.hidin"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	implementation("org.springframework.boot:spring-boot-starter")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
---
+++ src/main/java/de/hidin/linktreebackendservice/LinktreeBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package de.hidin.linktreebackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinktreeBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinktreeBackendServiceApplication.class, args);
+	}
+
+}
